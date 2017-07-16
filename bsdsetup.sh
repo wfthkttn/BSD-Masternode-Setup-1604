dbd851358ca6d0b516447f82183ac19369d8d370
Jon Durdel <info@darkjon.de>
2017-07-16 16:32:51 +0200

    Create bsdsetup.sh

---
+++ bsdsetup.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+# This script will install all needed stuff to run a BitSend (BSD) Masternode.
+# BitSend Repo : https://github.com/LIMXTEC/BitSend
+# !! THIS SCRIPT NEED TO RUN AS ROOT !!
+######################################################################
+
+clear
+echo "*********** Welcome to the BitSend (BSD) Masternode Setup Script ***********"
+echo 'This script will install all needed updates & package for Ubuntu 14.04 !'
+echo '****************************************************************************'
+sleep 5
