c9ea2ea76c0e607025d7008b32b157318f4c83b8
wfthkttn <wfthkttn@gmail.com>
2017-12-11 07:03:49 +0300

    Update bsdsetup.sh

--- bsdsetup.sh
+++ bsdsetup.sh
@@ -14,9 +14,10 @@ sleep 3
 echo '*** Step 1/5 ***'
 echo '*** Creating 2GB Swapfile ***'
 sleep 1
-dd if=/dev/zero of=/mnt/mybsdswap.swap bs=2M count=1000
-mkswap /mnt/mybsdswap.swap
-swapon /mnt/mybsdswap.swap
+sudo dd if=/dev/zero of=/swapfile bs=1M count=2048
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo chmod 600 /swapfile
 sleep 1
 echo '*** Done 1/5 ***'
 sleep 1
@@ -25,15 +26,14 @@ echo '*** Running updates and install required packages ***'
 sleep 2
 sudo apt-get update -y
 sudo apt-get dist-upgrade -y
-sudo apt-get install build-essential libtool autotools-dev autoconf pkg-config libssl-dev -y
+sudo apt-get install build-essential libtool autotools-dev autoconf automake pkg-config libssl-dev -y
 sudo apt-get install libboost-all-dev git npm nodejs nodejs-legacy libminiupnpc-dev redis-server -y
 sudo apt-get install software-properties-common -y
 sudo apt-get install libevent-dev -y
-add-apt-repository ppa:bitcoin/bitcoin
-apt-get update -y
-apt-get install libdb4.8-dev libdb4.8++-dev -y
-curl https://raw.githubusercontent.com/creationix/nvm/v0.16.1/install.sh | sh
-source ~/.profile
+sudo add-apt-repository ppa:bitcoin/bitcoin
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
 echo '*** Done 2/5 ***'
 sleep 1
 echo '*** Step 3/5 ***'
@@ -49,7 +49,14 @@ cd
 cd BitSend/src
 strip bitsendd
 cp bitsendd /usr/local/bin
-/sbin/iptables -A INPUT -i eth0 -p tcp --dport 8886 -j ACCEPT
+strip bitsend-cli
+cp bitsend-cli /usr/local/bin
+sudo ufw allow ssh/tcp
+sudo ufw limit ssh/tcp
+sudo ufw allow 8886/tcp
+sudo ufw default deny incoming 
+sudo ufw default allow outgoing 
+sudo ufw enable -y
 cd
 
 echo '*** Done 3/5 ***'
@@ -57,16 +64,13 @@ sleep 2
 echo '*** Step 4/5 ***'
 echo '*** Configure bitsend.conf and download and import bootstrap file ***'
 sleep 2
-
 bitsendd
 sleep 3
-
-echo -n "Please Enter a STRONG Password or copy & paste the password generated for you above and Hit [ENTER]: "
-read usrpas
+echo -e "[Unit]\nDescription=BitSend's distributed currency daemon\nAfter=network.target\n\n[Service]\nUser=bitsend\nGroup=bitsend\n\nType=forking\nPIDFile=/home/bitsend/.bitsend/bitsendd.pid\n\nExecStart=/usr/local/bin/bitsendd -daemon -disablewallet -pid=/home/bitsend/.bitsend/bitsendd.pid \\n          -conf=/home/bitsend/.bitsend/bitsend.conf -datadir=/home/bitsend/.bitsend/\n\nExecStop=-/usr/local/bin/bitsend-cli -conf=/home/bitsend/.bitsend/bitsend.conf \\n         -datadir=/home/bitsend/.bitsend/ stop\n\nRestart=always\nPrivateTmp=true\nTimeoutStopSec=60s\nTimeoutStartSec=2s\nStartLimitInterval=120s\nStartLimitBurst=5\n\n[Install]\nWantedBy=multi-user.target\n" > /usr/lib/systemd/system/bitsendd.service
+sleep 2
 echo -n "Please Enter your masternode genkey respond and Hit [ENTER]: "
 read mngenkey
-
-echo -e "rpcuser=bsdmasternodeservice2387645 \nrpcpassword=$usrpas \nrpcallowip=127.0.0.1 \nserver=1 \nlisten=1 \ndaemon=1 \nlogtimestamps=1 \nmasternode=1 \npromode=1 \nmasternodeprivkey=$mngenkey \naddnode=107.170.2.241 \naddnode=45.58.51.22 \naddnode=104.207.131.249 \naddnode=68.197.13.94 \naddnode=109.30.168.16 \naddnode=31.41.247.133 \naddnode=37.120.190.76 \n" > ~/.bitsend/bitsend.conf
+echo -e "rpcuser=bsdmasternodefaceRolliNgForUseRnamEpjasda4567 \nrpcpassword=$(openssl rand -base64 32) \nrpcallowip=127.0.0.1 \nserver=1 \nlisten=1 \ndaemon=1 \nlogtimestamps=1 \nmasternode=1 \npromode=1 \nmasternodeprivkey=$mngenkey \naddnode=107.170.2.241 \naddnode=45.58.51.22 \naddnode=104.207.131.249 \naddnode=68.197.13.94 \naddnode=109.30.168.16 \naddnode=31.41.247.133 \naddnode=37.120.190.76 \n" > ~/.bitsend/bitsend.conf
 cd .bitsend
 
 wget https://www.mybitsend.com/bootstrap.tar.gz
@@ -77,7 +81,7 @@ sleep 2
 echo '*** Step 5/5 ***'
 echo '*** Last Server Start also Wallet Sync ***'
 echo 'After 1 minute you will see the 'getinfo' output from the RPC Server...'
-bitsendd
+systemctl start bitsendd
 sleep 60
 bitsendd getinfo
 sleep 2
