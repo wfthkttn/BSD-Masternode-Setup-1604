4bb52eb29c8122651b1f83667a55d7f1b309caca
Alexander Usmanov <wfthkttn@gmail.com>
2017-12-15 21:51:54 +0300

    Update bsdsetup.sh

--- bsdsetup.sh
+++ bsdsetup.sh
@@ -12,10 +12,14 @@ echo 'Create specific user to handle masternode, set firewall options and add bi
 echo 'Clone & Compile the BSD Wallet also help you on first setup and sync'
 echo '****************************************************************************'
 sleep 2
-echo '*** Step 1/10 - creating bitsend user ***'
-sleep 2
-echo -n "Please Enter new password for [bitsend] user and Hit [ENTER]: "
+echo '*** Step 0/10 - User input ***'
+echo -n "Enter new password for [bitsend] user and Hit [ENTER]: "
 read BSDPASSWORD
+sleep 2
+echo -n "Enter your masternode genkey respond and Hit [ENTER]: "
+read mngenkey
+sleep 2
+echo '*** Step 1/10 - creating bitsend user ***'
 sleep 2
 adduser --disabled-password --gecos "" bitsend 
 usermod -a -G sudo bitsend
@@ -74,16 +78,13 @@ ufw limit ssh/tcp
 ufw allow 8886/tcp
 ufw default deny incoming 
 ufw default allow outgoing 
-echo 'PRESS y & ENTER!'
-ufw enable
+yes | ufw enable
 sleep 2
 echo '*** Done 5/10 ***'
 sleep 2
 echo '*** Step 6/10 - Configure bitsend.conf ***'
 sleep 2
 sudo -u bitsend mkdir /home/bitsend/.bitsend
-echo -n "Please Enter your masternode genkey respond and Hit [ENTER]: "
-read mngenkey
 sudo -u bitsend echo -e "rpcuser=bsdmasternode$(openssl rand -base64 32) \nrpcpassword=$(openssl rand -base64 32) \nrpcallowip=127.0.0.1 \nserver=1 \nlisten=1 \ndaemon=1 \nlogtimestamps=1 \nmasternode=1 \npromode=1 \nmasternodeprivkey=$mngenkey \naddnode=107.170.2.241 \naddnode=45.58.51.22 \naddnode=104.207.131.249 \naddnode=68.197.13.94 \naddnode=109.30.168.16 \naddnode=31.41.247.133 \naddnode=37.120.190.76 \n" > /home/bitsend/.bitsend/bitsend.conf
 cd /home/bitsend/.bitsend
 sleep 2
@@ -114,9 +115,12 @@ sleep 2
 echo '*** Done 9/10 ***'
 sleep 2
 echo '*** Step 10/10 - Get ready for network rewards! ***'
-sleep 2
-echo 'Visit our telegram channel at t.me/BSD_bitsend'
 sleep 4
-echo 'Now look until blocks get fully synchronized, then enable masternode on local wallet! :3'
-sleep 30
+echo 'You need to reboot server after you see message like:'
+echo '2018-01-32 24:61:61 UpdateTip: new best=0000000001602844h6h46649ab3cc7d66969e80b2cd970773d355a97bb9ac height=407633 version=0x20000000 log2_work=55.377649 tx=570794 date='2017-12-20 16:26:23' progress=1.000000 cache=0.0MiB(188tx)'
+echo 'where "height" equals to "Current numbers of blocks" in local wallet (help>debug>information).'
+echo 'After server restarts - you are free to enable masternode on local wallet.'
+sleep 20
+echo 'Now go, and visit our telegram channel at t.me/BSD_bitsend, tell us how its going!'
+sleep 100
 tail -f debug.log
