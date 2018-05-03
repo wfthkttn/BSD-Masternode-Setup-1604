0e7d8a2c10b0d21f76a1775f274c95b9835ca4ce
David B <37987466+dalijolijo@users.noreply.github.com>
2018-05-03 16:02:24 +0200

    Change to https://www.mybitsend.com

--- start.sh
+++ start.sh
@@ -16,8 +16,8 @@ sed -i "s/^\(masternodeprivkey=\).*/\masternodeprivkey=${MN_KEY}/" /home/bitsend
 #
 printf "** Step 8/10 - Downloading bootstrap file ***"
 cd /home/bitsend/.bitsend
-if [ ! -f /home/bitsend/.bitsend/${BOOTSTRAP} ] && [ "$(curl -Is 207.246.121.232:1337/${BOOTSTRAP} | head -n 1 | tr -d '\r\n')" = "HTTP/1.1 200 OK" ] ; then \
-        sudo -u bitsend wget 207.246.121.232:1337/$BOOTSTRAP; \
+if [ ! -f /home/bitsend/.bitsend/${BOOTSTRAP} ] && [ "$(curl -Is https://www.mybitsend.com/${BOOTSTRAP} | head -n 1 | tr -d '\r\n')" = "HTTP/1.1 200 OK" ] ; then \
+        sudo -u bitsend wget https://www.mybitsend.com/$BOOTSTRAP; \
         sudo -u bitsend tar -xvzf $BOOTSTRAP; \
         rm $BOOTSTRAP; \
 fi
