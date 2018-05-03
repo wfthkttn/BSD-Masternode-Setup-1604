a2cf0a4fff7cbed39df403c51d87b20cce9599fb
David B <37987466+dalijolijo@users.noreply.github.com>
2018-05-04 00:37:26 +0200

    Add check if blocks dir exists

--- start.sh
+++ start.sh
@@ -15,11 +15,10 @@ sed -i "s/^\(masternodeprivkey=\).*/\masternodeprivkey=${MN_KEY}/" /home/bitsend
 # Step 8/10 - Downloading bootstrap file
 #
 printf "** Step 8/10 - Downloading bootstrap file ***"
-cd /home/bitsend/.bitsend
-if [ "$(curl -Is https://www.mybitsend.com/${BOOTSTRAP} | head -n 1 | tr -d '\r\n')" = "HTTP/1.1 200 OK" ] ; then \
+cd /home/bitsend/.bitsend/
+if if [ ! -d /home/bitsend/.bitsend/blocks ] && [ "$(curl -Is https://www.mybitsend.com/${BOOTSTRAP} | head -n 1 | tr -d '\r\n')" = "HTTP/1.1 200 OK" ] ; then \
         sudo -u bitsend wget https://www.mybitsend.com/$BOOTSTRAP; \
         sudo -u bitsend tar -xvzf $BOOTSTRAP; \
-        rm $BOOTSTRAP; \
 fi
 printf "*** Done 8/10 ***"
 
