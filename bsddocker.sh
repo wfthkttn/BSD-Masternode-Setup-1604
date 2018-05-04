01abdb33af2f8623643f97ada0686cc74aaf7f6e
David B <37987466+dalijolijo@users.noreply.github.com>
2018-05-04 17:15:47 +0200

    disable distro check

--- bsddocker.sh
+++ bsddocker.sh
@@ -16,14 +16,14 @@ read MN_KEY
 # Check distro version (TODO)
 #
 #cat /etc/issue
-echo 'Checking OS version.'
-if [[ -r /etc/os-release ]]; then
-		. /etc/os-release
-		if [[ "${VERSION_ID}" != "16.04" ]]; then
-			echo "This script only supports ubuntu 16.04 LTS, exiting."
-			exit 1
-		fi
-fi
+#echo 'Checking OS version.'
+#if [[ -r /etc/os-release ]]; then
+#		. /etc/os-release
+#		if [[ "${VERSION_ID}" != "16.04" ]]; then
+#			echo "This script only supports ubuntu 16.04 LTS, exiting."
+#			exit 1
+#		fi
+#fi
 
 #
 # Firewall settings
