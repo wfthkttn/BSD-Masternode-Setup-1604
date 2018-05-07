7b34bed8806fd8764f64254154b30776b754eaa8
David B <37987466+dalijolijo@users.noreply.github.com>
2018-05-07 16:09:02 +0200

    Update bsddocker.sh

--- bsddocker.sh
+++ bsddocker.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -u
 
-DOCKER_REPO='dalijolijo'
+DOCKER_REPO="dalijolijo"
 
 #
 # Set bitsend user pwd and masternode genkey
 #
 echo '*** Step 0/10 - User input ***'
 echo -n "Enter new password for [bitsend] user and Hit [ENTER]: "
-read PWD
+read BSDPWD
 echo -n "Enter your masternode genkey respond and Hit [ENTER]: "
 read MN_KEY
 
@@ -43,7 +43,6 @@ yes | ufw enable
 #
 apt-get update
 apt-get upgrade -y
-apt-get install docker.io
 apt-get install docker.io -y
 docker pull ${DOCKER_REPO}/bsd-masternode
-docker run -p 8886:8886 -p 8800:8800 --name bsd-masternode -e BSDPWD='${PWD}' -e MN_KEY='${MN_KEY}' -v /home/bitsend:/home/bitsend:rw -d ${DOCKER_REPO}/bsd-masternode
+docker run --rm -p 8886:8886 -p 8800:8800 --name bsd-masternode -e BSDPWD="${BSDPWD}" -e MN_KEY="${MN_KEY}" -v /home/bitsend:/home/bitsend:rw ${DOCKER_REPO}/bsd-masternode
