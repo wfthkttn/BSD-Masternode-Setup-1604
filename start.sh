915a829ae989348a80f8a1e37e2d16db515d9b4d
David B <37987466+dalijolijo@users.noreply.github.com>
2018-05-16 16:12:28 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,15 +11,16 @@ echo bitsend:${BSDPWD} | chpasswd
 #
 # Set masternode genkey
 #
+printf "** Set masternode genkey ***"
 mkdir -p /home/bitsend/.bitsend
 chown -R bitsend:bitsend /home/bitsend
 sudo -u bitsend cp /tmp/bitsend.conf /home/bitsend/.bitsend/
 sed -i "s/^\(masternodeprivkey=\).*/\masternodeprivkey=${MN_KEY}/" /home/bitsend/.bitsend/bitsend.conf
 
 #
-# Step 8/10 - Downloading bootstrap file
+# Downloading bootstrap file
 #
-printf "** Step 8/10 - Downloading bootstrap file ***"
+printf "** Downloading bootstrap file ***"
 cd /home/bitsend/.bitsend/
 if [ ! -d /home/bitsend/.bitsend/blocks ] && [ "$(curl -Is https://www.mybitsend.com/${BOOTSTRAP} | head -n 1 | tr -d '\r\n')" = "HTTP/1.1 200 OK" ] ; then \
         sudo -u bitsend wget https://www.mybitsend.com/${BOOTSTRAP}; \
@@ -29,8 +30,8 @@ fi
 printf "*** Done 8/10 ***"
 
 #
-# Step 9/10 - Starting BitSend Service
+# Step Starting BitSend Service
 #
 # Hint: docker not supported systemd, use of supervisord
-printf "*** Step 9/10 - Starting BitSend Service ***"
+printf "*** Starting BitSend Service ***"
 exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
