c338f9c27c11f7f02784661df4d7a5faf3e3cb9b
David B <37987466+dalijolijo@users.noreply.github.com>
2018-05-19 12:10:53 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,9 +11,10 @@ echo bitsend:${BSDPWD} | chpasswd
 #
 # Set rpcuser, rpcpassword and masternode genkey
 #
-printf "** Set rpcuser, rpcpassword and masternode genkey ***"
+printf "** Set rpcuser, rpcpassword and masternode genkey ***\n"
 mkdir -p /home/bitsend/.bitsend
 chown -R bitsend:bitsend /home/bitsend
+chown bitsend:bitsend /tmp/bitsend.conf
 sudo -u bitsend cp /tmp/bitsend.conf /home/bitsend/.bitsend/
 sed -i "s/^\(rpcuser=\).*/\rpcuser=bsdmasternode${BSDPWD}/" /home/bitsend/.bitsend/bitsend.conf
 sed -i "s/^\(rpcpassword=\).*/\rpcpassword=${BSDPWD}/" /home/bitsend/.bitsend/bitsend.conf
@@ -22,7 +23,7 @@ sed -i "s/^\(masternodeprivkey=\).*/\masternodeprivkey=${MN_KEY}/" /home/bitsend
 #
 # Downloading bootstrap file
 #
-printf "** Downloading bootstrap file ***"
+printf "** Downloading bootstrap file ***\n"
 cd /home/bitsend/.bitsend/
 if [ ! -d /home/bitsend/.bitsend/blocks ] && [ "$(curl -Is https://www.mybitsend.com/${BOOTSTRAP} | head -n 1 | tr -d '\r\n')" = "HTTP/1.1 200 OK" ] ; then \
         sudo -u bitsend wget https://www.mybitsend.com/${BOOTSTRAP}; \
@@ -34,5 +35,5 @@ fi
 # Step Starting BitSend Service
 #
 # Hint: docker not supported systemd, use of supervisord
-printf "*** Starting BitSend Service ***"
+printf "*** Starting BitSend Service ***\n"
 exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
